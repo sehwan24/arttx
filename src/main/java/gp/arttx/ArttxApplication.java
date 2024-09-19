c77264647f9f4d64efc8d7ee92c25a302d6a340e
sehwan24 <1224ssanta@naver.com>
2024-09-19 15:49:23 +0900

    first commit

---
+++ src/main/java/gp/arttx/ArttxApplication.java
@@ -0,0 +1,13 @@
+package gp.arttx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArttxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArttxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gp/arttx/ArttxApplicationTests.java
@@ -0,0 +1,13 @@
+package gp.arttx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArttxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
