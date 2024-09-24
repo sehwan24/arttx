94434e72b155490d77b46c551501ed1abeeef05c
sehwan24 <1224ssanta@naver.com>
2024-09-25 08:35:47 +0900

    [fix] SecurityConfig 수정

--- src/main/java/gp/arttx/controller/TestController.java
+++ src/main/java/gp/arttx/controller/TestController.java
@@ -1,10 +1,12 @@
 package gp.arttx.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api/test")
 @RestController
 public class TestController {
