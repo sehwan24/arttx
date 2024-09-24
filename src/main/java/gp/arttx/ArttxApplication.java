8f080868902f1f0b9f7f55a1718b733b561331e6
sehwan24 <1224ssanta@naver.com>
2024-09-25 07:44:01 +0900

    [feat] 테스트용 Api 개발

--- src/main/java/gp/arttx/ArttxApplication.java
+++ src/main/java/gp/arttx/ArttxApplication.java
@@ -3,7 +3,7 @@ package gp.arttx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityException.class)
 public class ArttxApplication {
 
 	public static void main(String[] args) {
