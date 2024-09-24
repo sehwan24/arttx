8643a2485edaaa0b9c5d4df109b95e1964937889
sehwan24 <1224ssanta@naver.com>
2024-09-25 06:23:17 +0900

    [feat] 테스트용 Api 개발

--- src/main/java/gp/arttx/controller/TestController.java
+++ src/main/java/gp/arttx/controller/TestController.java
@@ -1,9 +1,11 @@
 package gp.arttx.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/test")
 @RestController
 public class TestController {
