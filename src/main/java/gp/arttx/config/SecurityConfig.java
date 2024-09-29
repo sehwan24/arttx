e922b5f256ac852f3b9cb724701e3b099236253d
sehwan24 <1224ssanta@naver.com>
2024-09-30 03:27:02 +0900

    [fix] SecurityConfig 수정

--- src/main/java/gp/arttx/config/SecurityConfig.java
+++ src/main/java/gp/arttx/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package gp.arttx.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -11,9 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,49 +26,38 @@ public class SecurityConfig {
 
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000","http://localhost:3000", "https://www.artpings.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setExposedHeaders(Arrays.asList("Authorization, Set-Cookie")); // CORS로 인해 프론트단에서 인식하지 못하는 Authrization 헤더를 노출
+        configuration.setAllowCredentials(true); // 쿠키 허용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://www.artpings.com");  // 허용할 도메인 추가
-        config.addAllowedMethod("*");  // 모든 HTTP 메서드 허용 (GET, POST, PUT 등)
-        config.addAllowedHeader("*");  // 모든 헤더 허용
-        config.setAllowCredentials(true);  // 자격 증명 허용 (쿠키, 인증 정보 등)
-
-        source.registerCorsConfiguration("/api/**", config);  // /api/** 경로에 CORS 설정 적용
-        return new CorsFilter(source);
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 적용
+        return source;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        http
-                .formLogin(AbstractHttpConfigurer::disable)// FormLogin 사용 X
-                .httpBasic(AbstractHttpConfigurer::disable)// httpBasic 사용 X
-                .csrf(AbstractHttpConfigurer::disable) // csrf 보안 사용 X
-                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource())); // CORS 설정 적용
 
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
 
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
-                //== URL별 권한 관리 옵션 ==//
-                .authorizeHttpRequests(authorize -> authorize
-                        // 아이콘, css, js 관련
-                        // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
-                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/favicon.ico")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/profile")).permitAll()
-                        //, "/js/**", "/favicon.ico", "/h2-console/**", "/profile")).permitAll()))
-                        //.requestMatchers("/api/member/sign-up").permitAll() // 회원가입 접근 가능
-                        //.requestMatchers("/auth/token").permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/auth/**")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/swagger-ui/**")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/v3/api-docs/**")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/*")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/chatting/**")).permitAll()  //임시 권한 부여
-                        .anyRequest().permitAll()); // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // OPTIONS 메소드 허용
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                                .requestMatchers("/api/users/login","/api/users/signup","/api/users/kakao/**","/api/users/naver/**","/api/campaigns/**").permitAll()
+                                .anyRequest().permitAll() // 그 외 모든 요청 인증처리
+                );
 
 
         return http.build();
