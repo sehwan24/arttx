c8995e90a8ff9b3ef0f787e905349404ddd356c4
sehwan24 <1224ssanta@naver.com>
2024-09-30 18:08:06 +0900

    [fix] security

--- src/main/java/gp/arttx/config/WebSecurityConfig.java
+++ src/main/java/gp/arttx/config/WebSecurityConfig.java
@@ -38,22 +38,6 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(corsConfigurationSource())); // CORS 설정 적용
 
-        // CSRF 설정
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
-        http.sessionManagement((sessionManagement) ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // OPTIONS 메소드 허용
-                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                                .requestMatchers("/api/users/login","/api/users/signup","/api/users/kakao/**","/api/users/naver/**","/api/campaigns/**").permitAll()
-                                .anyRequest().permitAll() // 그 외 모든 요청 인증처리
-                );
 
         return http.build();
     }
