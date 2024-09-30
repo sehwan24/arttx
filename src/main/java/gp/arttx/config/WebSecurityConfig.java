479459d329ba451b07835c9fab8b6ee5b2586407
sehwan24 <1224ssanta@naver.com>
2024-10-01 02:17:37 +0900

    [fix] security

--- src/main/java/gp/arttx/config/WebSecurityConfig.java
+++ src/main/java/gp/arttx/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package gp.arttx.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,6 +44,7 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)  // CSRF 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션 비활성화
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll());  // 모든 요청 허용
 
         return http.build();
