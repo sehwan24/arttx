544d71ba7ca46d608f3f77cbb68c025426cc0faf
sehwan24 <1224ssanta@naver.com>
2024-10-01 01:08:09 +0900

    [fix] security

--- src/main/java/gp/arttx/controller/TestController.java
+++ src/main/java/gp/arttx/controller/TestController.java
@@ -3,6 +3,8 @@ package gp.arttx.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,4 +20,11 @@ public class TestController {
         logger.info("INFO: /api/test endpoint hit");
         return "TestSuccess";
     }
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
