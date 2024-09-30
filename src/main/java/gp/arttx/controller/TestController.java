b241acbde16ecc098520fd7a160501c0020b249c
sehwan24 <1224ssanta@naver.com>
2024-09-30 17:49:18 +0900

    [fix] log

--- src/main/java/gp/arttx/controller/TestController.java
+++ src/main/java/gp/arttx/controller/TestController.java
@@ -1,6 +1,8 @@
 package gp.arttx.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/1")
     public String testReturn() {
+        logger.debug("DEBUG: /api/test endpoint hit");
+        logger.info("INFO: /api/test endpoint hit");
         return "TestSuccess";
     }
 }
