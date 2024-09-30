0d27095e7df8a94a692c4fb156cc31ffefd4eb39
sehwan24 <1224ssanta@naver.com>
2024-09-30 17:55:17 +0900

    [fix] log

--- src/main/java/gp/arttx/controller/TestController.java
+++ src/main/java/gp/arttx/controller/TestController.java
@@ -3,10 +3,7 @@ package gp.arttx.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/api/test")
@@ -15,6 +12,13 @@ public class TestController {
 
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
+    @CrossOrigin(origins = "https://www.artpings.com")
+    @RequestMapping(value = "/1", method = RequestMethod.OPTIONS)
+    public void options() {
+        // Preflight 요청 처리
+    }
+
+    @CrossOrigin(origins = "https://www.artpings.com")
     @GetMapping("/1")
     public String testReturn() {
         logger.debug("DEBUG: /api/test endpoint hit");
