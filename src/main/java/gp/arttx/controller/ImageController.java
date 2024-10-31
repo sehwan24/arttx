69f4c78aed9d20cb8ad0f67592d27d8fdae8c34a
sehwan24 <1224ssanta@naver.com>
2024-10-31 12:59:12 +0900

    [feat] S3 File Upload

---
+++ src/main/java/gp/arttx/controller/ImageController.java
@@ -0,0 +1,13 @@
+package gp.arttx.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/image")
+public class ImageController {
+
+
+}
